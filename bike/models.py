9e5896915704d8d4db8dfe6a376ae4712720ca71
blazejosojca <blazejosojca@gmail.com>
2017-11-25 11:15:01 +0100

    first commit -create project and apps

---
+++ bike/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+TYPES = (
+    ("0", "unknowns"),
+    ("1", "city bike"),
+    ("2", "cross bike"),
+    ("3", "trekking bike"),
+    ("4", "mountain terrain bike"),
+    ("5", "bike for kids")
+)
+
+
+class Bike(models.Model):
+    producer_name = models.CharField(max_length=64)
+    model_name = models.CharField(max_length=64)
+    frame_number = models.IntegerField(default=0)
+    bike_type = models.IntegerField(choices=TYPES)
+    is_rented = models.BooleanField(default=False)
+    is_functional = models.BooleanField(default=True)
+
