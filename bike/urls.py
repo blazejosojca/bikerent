737781e2fd7e5e2671c675facb7e4bee7c5fd822
blazejosojca <blazejosojca@gmail.com>
2017-11-25 14:28:20 +0100

    added functionality adding bike

--- bike/urls.py
+++ bike/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^bikes_list/$', views.BikesListView.as_view(), name="bikes_list")
+    url(r'^bikes_list/$', views.BikesListView.as_view(), name="bikes_list"),
+    url(r'^add_bike/$', views.AddingBikeFormView.as_view(), name="add_bike")
 ]
--- bike/views.py
+++ bike/views.py
@@ -3,14 +3,27 @@ from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.views.generic import (
     ListView,
+    CreateView,
+    FormView,
     View
 )
 
 from .models import Bike, Localization
+from .forms import AddingBikeForm
+
 
 class BikesListView(ListView):
     template_name = "bike/bikes_list.html"
     ctx = "bike_list"
-    
+
     def get_queryset(self):
         return Bike.objects.all()
+
+
+class AddingBikeFormView(CreateView):
+    template_name = 'bike/bike_form.html'
+    model = Bike
+    fields = ['producer_name',
+              'model_name',
+              'frame_number',
+              'bike_type']
