40a4d6fb4ca50df9fe775cb8ec99bd6af2f406de
blazejosojca <blazejosojca@gmail.com>
2017-11-26 15:39:22 +0100

    added empty view classes to client app

--- client/urls.py
+++ client/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^clients/login/$', views.LoginClientView.as_view(), name="client-login"),
     url(r'^clients/logout/(?P<pk>[0-9]+)/$', views.LogoutClientView.as_view(), name="client-logout"),
     url(r'^clients/account/(?P<pk>[0-9]+)/$', views.ViewAccountClientView.as_view(), name="client-account"),
-    url(r'^clients/update/(?P<pk>[0-9]+)/$', views.UpdateAccountCleintView.as_view(), name="client-update"),
+    url(r'^clients/update/(?P<pk>[0-9]+)/$', views.UpdateAccountClientView.as_view(), name="client-update"),
     url(r'^clients/delete_account/(?P<pk>[0-9]+)/$', views.DeleteAccountClientView.as_view(), name="client-delete"),
 ]
--- client/views.py
+++ client/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
 from .models import Client
 from django.template.response import TemplateResponse
-from bike.models import Bike, Localization
-from django.views.generic import ListView, View
+from django.views.generic import (
+    ListView,
+    View,
+    DetailView,
+    DeleteView,
+    UpdateView,
 
-"""
-urlpatterns = [
-    url(r'^clients/$', views.StartClientView.as_view(), name="client-start"),
-    url(r'^clients/register/$', views.RegisterClientView.as_view(), name="client-register"),
-    url(r'^clients/login/$', views.LoginClientView.as_view(), name="client-login"),
-    url(r'^clients/logout/(?P<pk>[0-9]+)/$', views.LogoutClientView.as_view(), name="client-logout"),
-    url(r'^clients/account/(?P<pk>[0-9]+)/$', views.ViewAccountClientView.as_view(), name="client-account"),
-    url(r'^clients/update/(?P<pk>[0-9]+)/$', views.UpdateAccountCleintView.as_view(), name="client-update"),
-    url(r'^clients/delete_account/(?P<pk>[0-9]+)/$', views.DeleteAccountClientView.as_view(), name="client-delete"),
-]
-"""
+)
+
+
+class StartClientView(View):
+    pass
+
+
+class RegisterClientView(View):
+    pass
+
+
+class LoginClientView(View):
+    pass
+
+
+class LogoutClientView(View):
+    pass
+
+
+class ViewAccountClientView(DetailView):
+    pass
+
+
+class UpdateAccountClientView(UpdateView):
+    pass
+
+
+class DeleteAccountClientView(DeleteView):
+    pass
